9c16cbe4a9c06445eb4e652582e8abe2e9e2da88
Akhil S Pillai <akhil@revemarketing.com>
2021-07-16 15:52:52 +0530

    Adds netlify support

---
+++ src/api.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const notificationsRouter = require("./routes/notifications");
+
+const app = express();
+
+// app.use("/", indexRouter);
+app.use("/.netlify/functions/api/notifications", notificationsRouter);
+
+module.exports.handler = serverless(app);
